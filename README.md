955d405534cca8101ca412effd65c4b86dbe2450
Dr. Daniel A Smith <daniel@danielsmith.eu>
2015-08-09 15:42:59 +0100

    Config, multiple URLs, pushbullet
    Config file support - examples shown
    Support for multiple URL watching
    Pushbullet support

--- README.md
+++ README.md
@@ -1,3 +1,31 @@
 # Thread Monitor
 
 Monitors a vBulletin forum and sends a desktop notification when a new thread is added.
+
+## Setup, config and usage
+
+I recommend using a virtualenv to handle the requirements, e.g.
+
+    virtualenv env
+    source env/bin/activate
+    pip install -r requirements.txt
+
+Then copy `config.json.osx` or `config.json.pushbullet` to `config.json` depending on which notification style you want.
+
+* OSX style is Desktop notification for the local machine's desktop.
+* Pushbullet is a multi-device cloud notification system, useful if you want to run this monitor on your own server and notify your phone/desktop etc.
+
+Then edit `config.json` to put in your watched thread URLs (there are examples from flyertalk.com already present).
+
+If you are using pushbullet, you need to copy your API token from [the pushbullet settings page](https://www.pushbullet.com/#settings/account) into the `pushbullet_access_token` value.
+
+Then run the monitor, e.g.:
+
+    python monitor.py
+
+If you want to run from another config file, you can supply it on the command line:
+
+    python monitor.py config2.json
+
+
+
--- monitor.py
+++ monitor.py
@@ -3,40 +3,55 @@ from pync import Notifier
 import requests
 import sys
 import time
+import json
+from pushbullet import Pushbullet
 
-if len(sys.argv) < 2:
-    print "You must specify the URL to monitor as an argument to the script"
-    sys.exit(1)
+config_file = "config.json"
+if len(sys.argv) == 2:
+    config_file = sys.argv[1];
 
-url = sys.argv[1]
-grp = "monitor-{0}".format(url)
+with open(config_file) as config_open:
+    config = json.load(config_open)
+
+grp = "monitor-{0}".format(config_file)
+out_type = config.get("output").lower()
+pb = None
+
+def output(text, link):
+    global pb
+    if out_type == "osx":
+        Notifier.notify(text, title = "New Thread Added", open = link, sound = "Submarine", group = grp, sender = "com.google.Chrome")
+    elif out_type == "pushbullet":
+        if pb is None:
+            pb = Pushbullet(config.get("pushbullet_access_token"))
+        pb.push_link(text, link)
 
 threads_seen = {}
 while True:
-    r = requests.get(url)
-    if (r.status_code != 200):
-        print "Error getting URL"
-    else:
-        b = BeautifulSoup(r.text, "html.parser")
-        found_forum = False
-        got_threads = False
-        for tr in b.findAll("tbody"):
-            forum_id = tr.attrs.get("id")
-            if forum_id and forum_id.startswith("threadbits_forum"):
-                found_forum = True
-                for td in tr.findAll("a"):
-                    thread_id = td.attrs.get("id")
-                    if thread_id and thread_id.startswith("thread_title"):
-                        if not threads_seen.get(thread_id):
-                            threads_seen[thread_id] = True
-                            print td.text
-                            link = td.attrs.get("href")
-                            Notifier.notify(td.text.encode("utf-8"), title = "New Thread Added", open = link, sound = "Submarine", group = grp, sender = "com.google.Chrome")
-                        got_threads = True
+    for url in config.get("watch"):
+        r = requests.get(url)
+        if (r.status_code != 200):
+            print "Error getting URL " + url
+        else:
+            b = BeautifulSoup(r.text, "html.parser")
+            found_forum = False
+            got_threads = False
+            for tr in b.findAll("tbody"):
+                forum_id = tr.attrs.get("id")
+                if forum_id and forum_id.startswith("threadbits_forum"):
+                    found_forum = True
+                    for td in tr.findAll("a"):
+                        thread_id = td.attrs.get("id")
+                        if thread_id and thread_id.startswith("thread_title"):
+                            if not threads_seen.get(thread_id):
+                                threads_seen[thread_id] = True
+                                output(td.text.encode("utf-8"), td.attrs.get("href"))
+                            got_threads = True
 
-        if not found_forum:
-            print "Couldn't find forum in HTML"
-        if not got_threads:
-            print "Couldn't find threads in HTML"
+            if not found_forum:
+                print "Couldn't find forum in HTML for " + url
+            if not got_threads:
+                print "Couldn't find threads in HTML for " + url
 
     time.sleep(60)
+
